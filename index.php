60bcbe151c49e6c5bda0c8aa71c76df31d605a9b
deusyvan <deusyvan@gmail.com>
2018-05-31 22:13:26 -0300

    Formulario

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <form method="POST" enctype="multipart/form-data">
 
+	Arquivo: <br/>
+	
+	<input type="file" name="arquivo"/><br/><br/>
+	
+	<input type="submit" value="Enviar Arquivos" />
 
 </form>
