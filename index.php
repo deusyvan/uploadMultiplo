b9279bfbeb7a53c6cc68dfec708516a27620602a
deusyvan <deusyvan@gmail.com>
2018-05-31 22:25:01 -0300

    Mostrando vários arquivos

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<?php 
+    print_r($_FILES);
+
+?>
+
 <form method="POST" enctype="multipart/form-data">
 
 	Arquivo: <br/>
 	
-	<input type="file" name="arquivo" multiple /><br/><br/>
+	<input type="file" name="arquivo[]" multiple /><br/><br/>
 	
 	<input type="submit" value="Enviar Arquivos" />
 
