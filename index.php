fc42375de1e51337c63f4aca39a2bc019a1c2ca5
deusyvan <deusyvan@gmail.com>
2018-05-31 23:02:18 -0300

    Especificando o arquivo para move-lo

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ if(isset($_FILES['arquivo'])){
         
         for ($q=0; $q<count($_FILES['arquivo']['tmp_name']);$q++){
             
-            $nomedoarquivo = md5($_FILES['arquivo']['name'].time().rand(0,999)).'.jpg';
+            $nomedoarquivo = md5($_FILES['arquivo']['name'][$q].time().rand(0,999)).'.jpg';
             
-            move_uploaded_file($_FILES['arquivo']['tmp_name'][$q], 'arquivos/');
+            move_uploaded_file($_FILES['arquivo']['tmp_name'][$q], 'arquivos/'.$nomedoarquivo);
         }
     }
     
